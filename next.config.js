df742463b3b82ab32c9f7e23254d0ca540e032f2
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-03-07 12:31:45 -0300

    Added scss support
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const path = require('path')
+
 module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   webpack: function(config) {
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
     })
     return config
-  }
+  },
 }
