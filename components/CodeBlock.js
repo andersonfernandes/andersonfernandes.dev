f983ab0b485b2fd4250c9818fcde15c50eeb2375
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-03-07 23:43:29 -0300

    Updated code block component
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -1,12 +1,35 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
+import styles from './CodeBlock.module.scss'
+
 export default function CodeBlock({ language, value }) {
+  const copyCode = (event) => {
+    const selection = document.createElement('textarea')
+    selection.value = value;
+    document.body.appendChild(selection);
+    selection.select();
+    document.execCommand('copy');
+    document.body.removeChild(selection);
+
+    event.target.innerHTML = 'Copied!'
+
+    setTimeout(() => {
+      event.target.innerHTML = 'Copy'
+    }, 1000)
+  }
+
   return (
-    <div style={{ width: '650px'}}>
-    <SyntaxHighlighter showLineNumbers={true} language={language} style={ darcula }>
-      {value}
-    </SyntaxHighlighter>
+    <div className={ styles['code-container'] }>
+      <div
+        className={ styles.copy }
+        onClick={ (event) => copyCode(event) }>
+        Copy
+      </div>
+
+      <SyntaxHighlighter showLineNumbers={true} language={language} style={ darcula }>
+        {value}
+      </SyntaxHighlighter>
     </div>
   )
 }
