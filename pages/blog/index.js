bab4e8a9e0d9cabcc4d0dc92473bef829404628c
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-05-26 16:06:36 -0300

    Add updated date to the posts list
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -17,7 +17,9 @@ export default function Blog({ meta, posts }) {
             key={post.slug}
             href={`/blog/${post.slug}`}>
             <div className={ styles['post-list-item'] }>
-              <div className={ styles.date }>{ formatedPublishedAt(post.date) }</div>
+              <div className={ styles.date }>
+                { post.updated ? `Updated at ${formatedPublishedAt(post.updated)}` : formatedPublishedAt(post.date) }
+              </div>
               <div className={ styles.title }>{ post.title }</div>
             </div>
           </Link>
