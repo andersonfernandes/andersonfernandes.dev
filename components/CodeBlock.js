df53234638a06ee2272346c02c8a3a07f900e3ae
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-08-24 23:49:45 -0300

    Update CodeBlock component with configuration directives
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -3,31 +3,44 @@ import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 import styles from './CodeBlock.module.scss'
 
-export default function CodeBlock({ language, value }) {
-  const copyCode = (event) => {
-    const selection = document.createElement('textarea')
-    selection.value = value;
-    document.body.appendChild(selection);
-    selection.select();
-    document.execCommand('copy');
-    document.body.removeChild(selection);
+const NO_COPY = '--no-copy'
+const NO_LINES = '--no-lines'
 
-    event.target.innerHTML = 'Copied!'
+const copyCode = (event, value) => {
+  const selection = document.createElement('textarea')
+  selection.value = value;
+  document.body.appendChild(selection);
+  selection.select();
+  document.execCommand('copy');
+  document.body.removeChild(selection);
 
-    setTimeout(() => {
-      event.target.innerHTML = 'Copy'
-    }, 1000)
-  }
+  event.target.innerHTML = 'Copied!'
+
+  setTimeout(() => {
+    event.target.innerHTML = 'Copy'
+  }, 1000)
+}
+
+const CopyButton = ({ value }) => {
+  return (
+    <div
+      className={ styles.copy }
+      onClick={ (event) => copyCode(event, value) }>
+      Copy
+    </div>
+  )
+}
+
+export default function CodeBlock({ language, value, node }) {
+  const meta = node.meta || ''
+  const copyEnabled = !meta.includes(NO_COPY)
+  const linesEnabled = !meta.includes(NO_LINES)
 
   return (
     <div className={ styles['code-container'] }>
-      <div
-        className={ styles.copy }
-        onClick={ (event) => copyCode(event) }>
-        Copy
-      </div>
+      { copyEnabled && <CopyButton value={value} /> }
 
-      <SyntaxHighlighter showLineNumbers={true} language={language} style={ darcula }>
+      <SyntaxHighlighter showLineNumbers={linesEnabled} language={language} style={ darcula }>
         {value}
       </SyntaxHighlighter>
     </div>
