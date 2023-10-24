d74ea852acc7fc054411b36d07cec99e045778cc
Anderson Fernandes <fernandesanderson14@gmail.com>
2023-10-24 12:53:11 -0300

    Format all files
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ components/BlogLayout.jsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import styles from "./BlogLayout.module.scss";
+
+export default function BlogLayout({ meta, pageMeta, children }) {
+  const router = useRouter();
+
+  let headTitle = meta.title;
+  let headDescription = meta.description;
+
+  if (pageMeta) {
+    headTitle = `${meta.title} - ${pageMeta.title}`;
+    headDescription = pageMeta.description;
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="Description" content={headDescription}></meta>
+        <title>{headTitle}</title>
+      </Head>
+
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <h1>{meta.title}</h1>
+          <h2>{meta.description}</h2>
+        </header>
+
+        <nav className={styles.navbar}>
+          {router.pathname != "/blog" ? backButton() : mainPageButton()}
+        </nav>
+
+        <main>{children}</main>
+      </div>
+    </>
+  );
+}
+
+function backButton() {
+  return <Link href={"/blog"}>{"< Back to all articles"}</Link>;
+}
+
+function mainPageButton() {
+  return <Link href={"/"}>{"< Back to main page"}</Link>;
+}
---
+++ pages/index.jsx
@@ -0,0 +1,77 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link
+          href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css"
+          rel="stylesheet"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <title>Anderson Fernandes - Software Engineer</title>
+      </Head>
+
+      <Image
+        src="/img/profile.jpg"
+        alt="Profile Image"
+        width="150"
+        height="150"
+        className={styles["profile-img"]}
+      />
+
+      <h1>Anderson Fernandes</h1>
+      <h2>Software Engineer</h2>
+
+      <div className={styles.about}>
+        <p>
+          I'm a Software Developer and Information Systems student at the
+          Federal Institute of Alagoas.
+        </p>
+        <p>
+          Currently working at{" "}
+          <Link href="https://clevertech.biz/" target="_BLANK">
+            Clevertech
+          </Link>
+          .
+        </p>
+      </div>
+
+      <div className={styles.social}>
+        <Link
+          href="mailto:fernandesanderson14@gmail.com"
+          title="fernandesanderson14@gmail.com"
+          target="_blank"
+        >
+          <i className="icon-envelope"></i>
+        </Link>
+
+        <Link
+          href="https://github.com/andersonfernandes/"
+          title="Github"
+          target="_blank"
+        >
+          <i className="icon-github"></i>
+        </Link>
+
+        <Link
+          href="https://br.linkedin.com/in/andersonfernandes12"
+          title="Linkedin"
+          target="_blank"
+        >
+          <i className="icon-linkedin"></i>
+        </Link>
+      </div>
+
+      <div className={styles.blog}>
+        <Link href="/blog" title="Blog">
+          Blog
+        </Link>
+      </div>
+    </div>
+  );
+}
