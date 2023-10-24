3e1944221ffa57f10004ddf1a13f9c4e247692c1
Anderson Fernandes <fernandesanderson14@gmail.com>
2023-10-24 16:11:37 -0300

    Update google tags setup
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
 import Head from "next/head";
+import Script from "next/script";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
