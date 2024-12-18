091c6d1dde34d7206d3b13a22c828c3b8b7bead7
Anderson Fernandes <fernandesanderson14@gmail.com>
2024-12-18 18:22:54 -0300

    Fix beacon
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,18 @@ function MyApp({ Component, pageProps }) {
                     });
                 `}
       </Script>
+
+      <Script strategy="lazyOnload">
+        {`
+!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
+                `}
+      </Script>
+
+      <Script strategy="lazyOnload">
+        {`
+window.Beacon('init', 'a1ce333e-3f2f-4c4f-a3bf-2f807acbf37d')
+                `}
+      </Script>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
